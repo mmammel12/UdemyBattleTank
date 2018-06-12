f178b14cedea86ae87c800796c8a1e6baab39e18
Marty <mmammel12@gmail.com>
2018-06-12 19:25:04 -0400

    BT 27 Udemy 132

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,5 +15,9 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 public:
 	virtual void BeginPlay() override;
 
+	virtual void Tick(float DeltaTime) override;
+
 	ATank* GetControlledTank() const;
+
+	void AimTowardsCrosshair();
 };

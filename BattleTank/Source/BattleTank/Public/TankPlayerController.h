d7ce3a66c9c9b240a89576b76ea9655370a7a879
Marty <mmammel12@gmail.com>
2018-06-15 19:23:17 -0400

    BT 33 Udemy 138

---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Public/Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" // must be last #include
+
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+private:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	ATank* GetControlledTank() const;
+
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+		float CrossHairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+		float CrossHairYLocation = 0.33333;
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 1000000.0;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+};

ffec16dc8a73b71129b6ae4deedc8f25c75443fe
Marty <mmammel12@gmail.com>
2018-06-16 16:03:25 -0400

    BT 39 Udemy 145

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -6,10 +6,8 @@
 #include "Components/StaticMeshComponent.h"
 #include "TankBarrel.generated.h"
 
-/**
- * 
- */
-UCLASS()
+
+UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
@@ -18,5 +16,13 @@ public:
 	void Elevate(float DegreesPerSecond);
 	
 private:
-	
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxDegreesPerSecond = 20.0;
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxElevationDegrees = 40.0;
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MinElevationDegrees = 0.0;
+
 };
